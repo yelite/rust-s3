f96c062bda61eaf6356cb1ef2aeaf60ffec0bb66
durch <durch@users.noreply.github.com>
2021-04-11 21:10:11 +0000

    deploy: ae166bad53c25c88b9d3784fb816783142400567

---
+++ awscreds/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["AssumeRoleWithWebIdentityResponse",""],["AssumeRoleWithWebIdentityResult",""],["AssumedRoleUser",""],["Credentials","AWS access credentials: access key, secret key, and optional token."],["ResponseMetadata",""],["StsResponseCredentials",""]]});
